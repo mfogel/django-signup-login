b025350be17def29858245b746e72a38a9c809e6
Mike Fogel <mike@fogel.ca>
2011-02-27 23:26:33 -0800

    modified BSD license, setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='django-signup-login',
+    version='0.1',
+    description='Django unified signup and login views.',
+    author='Mike Fogel',
+    author_email='mike@fogel.ca',
+    url='http://github.com/carbonXT/django-signup-login',
+    packages=['signup_login'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Utilities',
+    ],
+) 
